1b72666bf3f480fca72f6cd5a40a33d5b68cf2bc
Omar Houmz <omhoumz@gmail.com>
2020-03-15 19:56:16 +0100

    chore: center elements

--- components/wrapper.jsx
+++ components/wrapper.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Div = styled.div`
+  flex-grow: 1;
   max-width: 780px;
-  margin: auto;
+  margin-inline-start: auto;
+  margin-inline-end: auto;
+
+  padding: 1em;
 `
 
 const Wrapper = ({ children }) => {
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,13 @@ import Wrapper from '../components/wrapper'
 const GlobalStyles = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    margin: 0;
+    min-height: 100vh;
+  }
+
+  #__next {
+    min-height: 100vh;
+    display: flex;
   }
 `
 
